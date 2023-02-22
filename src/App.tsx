c4fbcc6069a52c15b95d1185505aada5baf97d83
Daryl Findlay <darylfindlay@gmail.com>
2023-02-22 22:06:31 +0000

    add mail to link

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App = () => {
         <section>
           <h1 className={classes.h1}>Hello, I&apos;m Daryl!</h1>
           <h2>Full Stack Software Engineer.</h2>
+          <a href="mailto:darylfindlay@gmail.com?subject=I'm%20interested%20in%20hiring%20you!&body=Hi%20Daryl%2C%20I'm%20interested%20in%20your%20work.%20Let's%20connect!" className={classes.contact}>Get In Touch</a>
           <div className={classes.desc}>
             <p>I&apos;m a software engineer with 10 years industry experience. Focus on frontend UI/UX design with modern FE frameworks and build tools like React, Vite and Nextjs. Graduated with Bachelor of Science in Computer Science from University of Ulster.</p>
             <p>JavaScript & TypeScript, NextJS, Vite, NodeJS, React, Express, Ruby on Rails and various testing packages.</p>
