8094d718adb3f0fb3bece91d257f7782bf91a6ee
Daryl Findlay <darylfindlay@gmail.com>
2023-02-26 21:14:27 +0000

    add banner

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const App = () => {
       <section className={classes.main}>
         <h1 className={classes.h1}>Hello, I&apos;m Daryl!</h1>
         <h2>Full Stack Software Engineer.</h2>
+        <h2>🚧 Under construction 🚧</h2>
         <a href="mailto:darylfindlay@gmail.com?subject=I'm%20interested%20in%20hiring%20you!&body=Hi%20Daryl%2C%20I'm%20interested%20in%20your%20work.%20Let's%20connect!" className={classes.contact}>Get In Touch</a>
         <div className={classes.desc}>
           <p>I&apos;m a software engineer with 10 years industry experience. Focus on frontend UI/UX design with modern FE frameworks and build tools like React, Vite and Nextjs. Graduated with Bachelor of Science in Computer Science from University of Ulster.</p>
