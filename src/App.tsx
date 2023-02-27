56e254976dd1abc61ab08e7a3eced56d01d79410
Daryl Findlay <darylfindlay@gmail.com>
2023-02-27 00:06:01 +0000

    gradient text, hiding firebase info

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,19 @@ const App = () => {
 
   return (
     <div className={classes.app}>
-      <br />
-      {user && (<><p>Welcome {user.displayName}</p><SignOut /></>)}
-      {!user && <SignInButton />}
+      <div className={classes.controls}>
+        {user && (<><p>Welcome {user.displayName}</p><SignOut /></>)}
+        {!user && <SignInButton />}
+      </div>
       <section className={classes.main}>
-        <h1 className={classes.h1}>Hello, I&apos;m Daryl!</h1>
+        <h1 className={classes.h1}>Hello, I&apos;m <span className={classes.name}>Daryl Findlay</span>!</h1>
         <h2>Full Stack Software Engineer.</h2>
         <h2>🚧 Under construction 🚧</h2>
         <a href="mailto:darylfindlay@gmail.com?subject=I'm%20interested%20in%20hiring%20you!&body=Hi%20Daryl%2C%20I'm%20interested%20in%20your%20work.%20Let's%20connect!" className={classes.contact}>Get In Touch</a>
-        <div className={classes.desc}>
+        {/* <div className={classes.desc}>
           <p>I&apos;m a software engineer with 10 years industry experience. Focus on frontend UI/UX design with modern FE frameworks and build tools like React, Vite and Nextjs. Graduated with Bachelor of Science in Computer Science from University of Ulster.</p>
           <p>JavaScript & TypeScript, NextJS, Vite, NodeJS, React, Express, Ruby on Rails and various testing packages.</p>
-        </div>
+        </div> */}
       </section>
     </div>
   );
